02185b7b68cb84e6bedd97d1c51c40916a92bfe5
tomtommahout <tomgrace@gmail.com>
2020-10-17 19:12:21 +1300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
  
 module1 = Extension('spi', sources = ['spi.c'])
  
-setup (name = 'rf905-py',
+setup (name = 'rf905_py',
         version = '1.0',
         description = 'This is a demo package',
         ext_modules = [module1])
