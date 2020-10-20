6f8aa25287e7a09946c11ff3ecae359eabefcbe3
tomtommahout <tomgrace@gmail.com>
2020-10-20 20:20:49 +1300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import RF905_Py
 
 rfn905 = RF905_Py.RF905(24,7,15,13,11,12,8)   #set  raspberryPi pin
 rfn905.writeconfig(0xCC,0xCC,0xCC,0xCC)       #set  sender RF905 address
-#rfn905.sendstr("hello",0xCC,0xCC,0xCC,0xCC,) #set  Receive Rf905 address
+rfn905.sendstr("hello",0xCC,0xCC,0xCC,0xCC,) #set  Receive Rf905 address
 
 def callback(str):
     print(str)
